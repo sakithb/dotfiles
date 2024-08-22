0c2a005d0619c6de23b77d7bf9b7f3d8f8ef0bb5
Sakith B. <pvsakith@gmail.com>
2024-08-22 14:09:04 +0530

    updates

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -5,32 +5,32 @@ return {
 			require("mason").setup()
 		end,
 	},
-	{
-		"coffebar/neovim-project",
-		lazy = false,
-		priority = 100,
-		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-telescope/telescope.nvim" },
-			{ "Shatur/neovim-session-manager" },
-		},
-		config = function()
-			require("neovim-project").setup({
-				projects = {
-					"~/Projects/*/*",
-				},
-			})
+	-- {
+	-- 	"coffebar/neovim-project",
+	-- 	lazy = false,
+	-- 	priority = 100,
+	-- 	dependencies = {
+	-- 		{ "nvim-lua/plenary.nvim" },
+	-- 		{ "nvim-telescope/telescope.nvim" },
+	-- 		{ "Shatur/neovim-session-manager" },
+	-- 	},
+	-- 	config = function()
+	-- 		require("neovim-project").setup({
+	-- 			projects = {
+	-- 				"~/Projects/*/*",
+	-- 			},
+	-- 		})
 
-			vim.keymap.set("n", "<leader>pp", ":Telescope neovim-project discover<CR>")
-			vim.keymap.set("n", "<leader>pr", ":Telescope neovim-project history<CR>")
+	-- 		vim.keymap.set("n", "<leader>pp", ":Telescope neovim-project discover<CR>")
+	-- 		vim.keymap.set("n", "<leader>pr", ":Telescope neovim-project history<CR>")
 
-			if vim.fn.argc() == 0 and vim.fn.expand("%") == vim.loop.cwd() then
-				vim.defer_fn(function()
-					vim.cmd(":Telescope neovim-project discover")
-				end, 0)
-			else
-				vim.cmd("cd " .. vim.fn.expand("%:p:h"))
-			end
-		end,
-	},
+	-- 		if vim.fn.argc() == 0 and vim.fn.expand("%") == vim.loop.cwd() then
+	-- 			vim.defer_fn(function()
+	-- 				vim.cmd(":Telescope neovim-project discover")
+	-- 			end, 0)
+	-- 		else
+	-- 			vim.cmd("cd " .. vim.fn.expand("%:p:h"))
+	-- 		end
+	-- 	end,
+	-- },
 }
