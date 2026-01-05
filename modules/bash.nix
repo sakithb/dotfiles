31fd9779a5eb8d8293ae77fc033952a51c38784b
Sakith B. <pvsakith@gmail.com>
2026-01-05 11:41:32 +0530

    05-01-26_11:41:32

--- modules/bash.nix
+++ modules/bash.nix
@@ -1,12 +1,12 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   programs.readline = {
     enable = true;
     variables = {
       editing-mode = "vi";
-      vi-ins-mode-string = "\"\\1\\e[6 q\\2\"";
-      vi-cmd-mode-string = "\"\\1\\e[2 q\\2\"";
+      vi-ins-mode-string = "+";
+      vi-cmd-mode-string = ":";
       keyseq-timeout = 10;
       completion-ignore-case = true;
       completion-map-case = true;
