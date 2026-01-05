60293884632e15c3a027ada59717f24b6e40af57
Sakith B. <pvsakith@gmail.com>
2026-01-05 11:36:14 +0530

    05-01-26_11:36:14

--- modules/bash.nix
+++ modules/bash.nix
@@ -5,7 +5,8 @@
     enable = true;
     variables = {
       editing-mode = "vi";
-      show-mode-in-prompt = true;
+	  vi-ins-mode-string = "\\1\\e[6 q\\2";
+	  vi-cmd-mode-string = "\\1\\e[2 q\\2";
       completion-ignore-case = true;
       completion-map-case = true;
       show-all-if-ambiguous = true;
