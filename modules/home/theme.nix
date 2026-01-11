ff48cfebdc91e087f26a310448e48cb74e55abf2
Sakith B. <pvsakith@gmail.com>
2026-01-11 13:07:14 +0530

    11-01-26_13:07:14

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -7,37 +7,37 @@
     };
   };
 
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-	name = "Adwaita";
-	package = pkgs.adwaita-icon-theme;
-    size = 16;
-  };
-
-  gtk = {
-    enable = true;
-    colorScheme = "dark";
-
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk";
-  };
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
+	#  home.pointerCursor = {
+	#    gtk.enable = true;
+	#    x11.enable = true;
+	# name = "Adwaita";
+	# package = pkgs.adwaita-icon-theme;
+	#    size = 16;
+	#  };
+	#
+	#  gtk = {
+	#    enable = true;
+	#    colorScheme = "dark";
+	#
+	#    theme = {
+	#      name = "Adwaita-dark";
+	#      package = pkgs.gnome-themes-extra;
+	#    };
+	#
+	#    iconTheme = {
+	#      name = "Papirus-Dark";
+	#      package = pkgs.papirus-icon-theme;
+	#    };
+	#  };
+	#
+	#  qt = {
+	#    enable = true;
+	#    platformTheme.name = "gtk";
+	#  };
+	#
+	#  dconf.settings = {
+	#    "org/gnome/desktop/interface" = {
+	#      color-scheme = "prefer-dark";
+	#    };
+	#  };
 }
