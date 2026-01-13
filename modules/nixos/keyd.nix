9dce2128b68985a12d1f279b4a7a0d2baf6f43c2
Sakith B. <pvsakith@gmail.com>
2026-01-13 08:13:32 +0530

    13-01-26_08:13:32

--- modules/nixos/keyd.nix
+++ modules/nixos/keyd.nix
@@ -6,12 +6,6 @@
     keyboards.default = {
       ids = [ "*" ];
       settings.main = {
-        shift = "oneshot(shift)";
-        meta = "oneshot(meta)";
-        control = "oneshot(control)";
-
-        leftalt = "oneshot(alt)";
-        rightalt = "oneshot(altgr)";
         capslock = "overloadt(control, esc, 200)";
         leftmeta = "overloadt(meta, f13, 200)";
       };
