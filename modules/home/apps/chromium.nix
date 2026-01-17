fb549e9774c1bd12f6e012bd1ee9c1e5f51165c3
Sakith B. <pvsakith@gmail.com>
2026-01-18 00:48:27 +0530

    18-01-26_00:48:27

--- modules/home/apps/chromium.nix
+++ modules/home/apps/chromium.nix
@@ -1,17 +1,59 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
-  programs.chromium = {
-    enable = true;
+  programs.chromium =
+    let
+      package = pkgs.ungoogled-chromium;
+      installExtensions = true;
+    in
+    {
+      inherit package;
 
-    package = pkgs.ungoogled-chromium.override {
-      enableWideVine = true;
+      enable = true;
+
+      commandLineArgs = [
+        "--extension-mime-request-handling=always-prompt-for-install"
+      ]
+      ++ lib.optionals installExtensions (
+        [
+          "https://github.com/NeverDecaf/chromium-web-store/releases/latest/download/Chromium.Web.Store.crx"
+        ]
+        ++ map (
+          extension:
+          "https://clients2.google.com/service/update2/crx?response=redirect\\&acceptformat=crx2,crx3\\&prodversion=${package.version}\\&x=id%3D${extension.id}%26uc"
+        ) config.programs.chromium.extensions
+      );
+
+      extensions = [
+        {
+          # I still don't care about cookies
+          id = "edibdbjcniadpccecjdfdjjppcpchdlm";
+        }
+        {
+          # KeePassXC-Browser
+          id = "oboonakemofpalcgghocfoadofidjkkk";
+        }
+        {
+          # Privacy Badger
+          id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp";
+        }
+        {
+          # Refined GitHub
+          id = "hlepfoohegkhhmjieoechaddaejaokhf";
+        }
+        {
+          # uBlock Origin Lite
+          id = "ddkjiahejlhfcafbddmgiahcphecmpfh";
+        }
+        {
+          # Wayback Machine
+          id = "fpnmgdkabkmnadcjpehmlllkndpkmiak";
+        }
+      ];
     };
-
-    extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Ublock
-      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # Vimium
-      { id = "oboonakemofpalcgghocfoadofidjkkk"; } # KeepassXC
-    ];
-  };
 }
