8cc168ee074ec9470aaac69b1ce808a01d5628bd
Sakith B. <pvsakith@gmail.com>
2026-01-18 18:16:23 +0530

    18-01-26_18:16:23

--- modules/nixos/keyd.nix
+++ modules/nixos/keyd.nix
@@ -7,48 +7,17 @@
       ids = [ "*" ];
       settings = {
         main = {
-          capslock = "overload(nav, esc)";
-
-          a = "overloadt(control, a, 200)";
-          s = "overloadt(alt, s, 200)";
-          d = "overloadt(meta, d, 200)";
-          f = "overloadt(shift, f, 200)";
-
-          j = "overloadt(shift, j, 200)";
-          k = "overloadt(meta, k, 200)";
-          l = "overloadt(alt, l, 200)";
-          ";" = "overloadt(control, ;, 200)";
-
-          leftalt = "layer(toggle_layer)";
+          capslock = "overload(ctrl, capslock)";
+          shift = "overload(shift, esc)";
+          tab = "overload(nav, tab)";
         };
-
-        nav = {
-          h = "left";
-          j = "down";
-          k = "up";
-          l = "right";
-
-          u = "pageup";
-          d = "pagedown";
-
-          x = "delete";
-          e = "backspace";
-
-          "0" = "home";
-          "4" = "end";
-        };
-
-        "toggle_layer" = {
-          esc = "toggle(gaming)";
-        };
-
-        gaming = {
-          leftalt = "layer(exit_game)";
-        };
-
-        "exit_game" = {
-          esc = "toggle(gaming)";
-        };
+		
+		nav = {
+		  h = "left";
+		  j = "down";
+		  k = "up";
+		  l = "right";
+		};
       };
     };
   };
