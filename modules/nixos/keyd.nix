eb2784373967c1b3dc6ea33232e171c23fd0650f
Sakith B. <pvsakith@gmail.com>
2026-01-18 18:06:16 +0530

    18-01-26_18:06:16

--- modules/nixos/keyd.nix
+++ modules/nixos/keyd.nix
@@ -5,9 +5,50 @@
     enable = true;
     keyboards.default = {
       ids = [ "*" ];
-      settings.main = {
-        capslock = "overloadt(control, esc, 200)";
-        leftmeta = "overloadt(meta, f13, 200)";
+      settings = {
+        main = {
+          capslock = "overload(nav, esc)";
+
+          a = "overloadt(control, a, 200)";
+          s = "overloadt(alt, s, 200)";
+          d = "overloadt(meta, d, 200)";
+          f = "overloadt(shift, f, 200)";
+
+          j = "overloadt(shift, j, 200)";
+          k = "overloadt(meta, k, 200)";
+          l = "overloadt(alt, l, 200)";
+          ";" = "overloadt(control, ;, 200)";
+
+          leftalt = "layer(toggle_layer)";
+        };
+
+        nav = {
+          h = "left";
+          j = "down";
+          k = "up";
+          l = "right";
+
+          u = "pageup";
+          d = "pagedown";
+
+          x = "delete";
+          e = "backspace";
+
+          "0" = "home";
+          "4" = "end";
+        };
+
+        "toggle_layer" = {
+          esc = "toggle(gaming)";
+        };
+
+        gaming = {
+          leftalt = "layer(exit_game)";
+        };
+
+        "exit_game" = {
+          esc = "toggle(gaming)";
+        };
       };
     };
   };
