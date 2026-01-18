a5c6a7696498ab84020a5410e055e1c38fd04038
Sakith B. <pvsakith@gmail.com>
2026-01-18 18:36:00 +0530

    18-01-26_18:36:00

--- modules/nixos/keyd.nix
+++ modules/nixos/keyd.nix
@@ -9,16 +9,32 @@
         main = {
           capslock = "overload(control, esc)";
           tab = "overload(nav, tab)";
+          rightalt = "layer(command_layer)";
         };
-		
-		nav = {
-		  h = "left";
-		  j = "down";
-		  k = "up";
-		  l = "right";
-		  u = "pageup";
-		  d = "pagedown";
-		};
+
+        command_layer = {
+          g = "toggle(gaming)";
+        };
+
+        gaming = {
+          tab = "tab";
+          capslock = "leftcontrol";
+
+          rightalt = "layer(command_layer)";
+        };
+
+        nav = {
+          h = "left";
+          j = "down";
+          k = "up";
+          l = "right";
+          u = "pageup";
+          d = "pagedown";
+          x = "delete";
+          w = "backspace";
+          b = "home";
+          e = "end";
+        };
       };
     };
   };
